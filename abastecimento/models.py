d8c4735d3e7fd12f654568cd4e7fa21e4fcc535b
alvarojoao <alvarojoao1@gmail.com>
2016-11-10 21:54:44 -0300

    inicio de nova base

--- abastecimento/models.py
+++ abastecimento/models.py
@@ -18,9 +18,132 @@ from django.contrib.auth.models import (
 
 # 	def __str__(self):              # __unicode__ on Python 2
 # 		return self.username
+class tipo(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+	unidade = models.CharField(max_length=200,primary_key=True)
+
+class familia(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	tipo = models.CharField(max_length=200,primary_key=True)
+	nome = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+	quantidadeDiaria = models.CharField(max_length=200,primary_key=True)
+	quantidadeDiaria = models.CharField(max_length=200,primary_key=True)
+
+class marca(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+
+class modelo(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	marca = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+	unidade = models.CharField(max_length=200,primary_key=True)
+
+class cor(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	cor = models.CharField(max_length=200,primary_key=True)
+
+class eixo(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+	numeroPneus = models.CharField(max_length=200,primary_key=True)
+
+class grupo(models.Model):
+	pass
+
+class ItemManutencao(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	grupo = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+	material = models.CharField(max_length=200,primary_key=True)
+	tempoServico = models.CharField(max_length=200,primary_key=True)
+	criado_date = models.DateField("Data Criada", auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado", blank=True, null=True,auto_now=True)
+
+class ItemManutencaoVeiculo(models.Model):
+	ItemManutencao = models.CharField(max_length=200,primary_key=True)
+	descricao = models.CharField(max_length=200,primary_key=True)
+	material = models.CharField(max_length=200,primary_key=True)
+	periodoPadrao = models.CharField(max_length=200,primary_key=True)
+	quantidade = models.CharField(max_length=200,primary_key=True)
+	unidade = models.CharField(max_length=200,primary_key=True)
+	tempoServico = models.CharField(max_length=200,primary_key=True)
+	criado_date = models.DateField("Data Criada", auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado", blank=True, null=True,auto_now=True)
+
+
+class ItemManutencaoProgramado(models.model):
+	ItemManutencaoVeiculo = models.CharField(max_length=200,primary_key=True)
+	hodometro = models.IntegerField('Hodômetro/Horimetro',default=0,validators= [validate_hodometro_and_veiculo_type])
+
+	criado_date = models.DateField("Data Criada", auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado", blank=True, null=True,auto_now=True)
+
+
+class ItemManutencaoNaoProgramado(models.model):
+	ItemManutencao = models.CharField(max_length=200,primary_key=True)
+	hodometro = models.IntegerField('Hodômetro/Horimetro',default=0,validators= [validate_hodometro_and_veiculo_type])
+
+	criado_date = models.DateField("Data Criada", auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado", blank=True, null=True,auto_now=True)
+
+class Fornecedor(models.Model):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	razaoSocial = models.CharField(max_length=200,primary_key=True)
+	nomeFantasia = models.CharField(max_length=200,primary_key=True)
+	cnpj = models.CharField(max_length=20,blank=True, null=True)
+	endereco = models.CharField(max_length=20,blank=True, null=True)
+	conceito = models.CharField(max_length=20,blank=True, null=True)
+	observacao = models.TextField( blank=True, null=True)
+	
+	criado_date = models.DateField("Data Criada",
+	        auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado",
+	        blank=True, null=True,auto_now=True)
+
+	def __str__(self):
+		return self.nome+' - cnpj: '+(str(self.cnpj) if self.cnpj else 'Nao Tem')
+
+class Locacao(models.Model):
+
+	obra = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	hodometroInicial = models.IntegerField('Hodômetro/Horimetro',default=0,validators= [validate_hodometro_and_veiculo_type])
+	hodometroFinal = models.IntegerField('Hodômetro/Horimetro',default=0,validators= [validate_hodometro_and_veiculo_type])
+	horasprodutivo = models.CharField(max_length=200,primary_key=True)
+	horasmanutencaoPreventiva = models.CharField(max_length=200,primary_key=True)
+	horasmanutencaoCorretiva = models.CharField(max_length=200,primary_key=True)
+	observacao = models.TextField( blank=True, null=True)
+
+	criado_date = models.DateField("Data Criada",
+	        auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado",
+	        blank=True, null=True,auto_now=True)
+
+class componente(model.Models):
+	codigo = models.CharField(max_length=200,primary_key=True)
+	nome = models.CharField(max_length=200,primary_key=True)
+
+#orcamento
+class custo(model.Models):
+	veiculo = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	notafiscal = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	data_emissao = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	componente = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	valor = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	fornecedor = models.ForeignKey(Obra,verbose_name="Obra envolvida no abastecimento",null=True)
+	
+	criado_date = models.DateField("Data Criada",
+	        auto_now_add=True)
+	atualizado_date = models.DateTimeField("Data Atualizado",
+	        blank=True, null=True,auto_now=True)
+
 
 class Obra(models.Model):
 	nome = models.CharField(max_length=200,primary_key=True)
+	status = models.CharField(max_length=200,primary_key=True)
+	sairnaagenda = models.CharField(max_length=200,primary_key=True)
 	criado_date = models.DateField("Data Criada", auto_now_add=True)
 	atualizado_date = models.DateTimeField("Data Atualizado", blank=True, null=True,auto_now=True)
 
